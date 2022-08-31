ce9fb285f32903b823e82b9ba837427fa778c2ac
jroiceee <jroicealdeza@gmail.com>
2022-08-31 16:59:23 +0800

    complete home page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,10 +3,20 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import "./home-main.scss";
 import fishImage from "./images/homepage-shop-fish.avif";
-import aquariumImage from "./images/homepage-shop-aquarium.avif"
+import backgroundImage from "./images/homepage-main.jpg";
+import aquariumImage from "./images/homepage-shop-aquarium.avif";
 import HomeRow from "./home-row";
 
 function Home() {
+  React.useEffect(() => {
+    document.body.style.backgroundColor = "#293241";
+    document.body.style.backgroundImage = `url(${backgroundImage})`;
+    return () => {
+      document.body.style.backgroundColor = "white";
+      document.body.style.backgroundImage = "none";
+    };
+  }, []);
+
   return (
     <Container fluid id="home-main" className="text-light">
       <Row className="text-center home-text-container">
@@ -15,17 +25,20 @@ function Home() {
           <h4>Delve into the world of ornamental fish keeping.</h4>
         </div>
       </Row>
-      <HomeRow
-        image={fishImage}
-        title="Fish"
-        description="From bettas to kois, we offer a wide variety of ornamental fish suitable for your
-            home and outdoor ponds and aquariums!"
-      />
-      <HomeRow
-        image={aquariumImage}
-        title="Aquariums"
-        description="Premium housing for your beloved fishes!"
-      />
+      <Row className="row-cols-md-2">
+        <HomeRow
+          image={fishImage}
+          title="Fish"
+          buttonLinkToPath="fish"
+          description="We offer a wide variety of ornamental fish suitable for your ponds and aquariums!"
+        />
+        <HomeRow
+          image={aquariumImage}
+          title="Aquariums"
+          buttonLinkToPath="aquariums"
+          description="Premium housing for your beloved fishes!"
+        />
+      </Row>
     </Container>
   );
 }
